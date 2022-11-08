4dffd932057b05b7291c09852b02748a9a0a3cd9
Pooky12332 <pooky12332@gmail.com>
2022-11-08 21:40:41 +0000

    Ver 1.7 - 08/11/22
    - Site is now fully released!
    - Fixed bug with 'tcol' and 'bcol' that caused them to clear all colours
    - Added new command section 'apps' for containing all apps
    - Added cookies to 'carg' (test run)
    - Added apps folder for apps that I make in the future
    - Added 'clog' to main commands, it displays the change log in the terminal
    - Moved 'tcol' and 'bcol' to fun
    - Moved 'python' and 'fun' to app commands
    - Moved 'carg' to system commands
    - Moved 'theme' to system commands
    - Removed '--animtest' from 'fun'
    - Changed 'argc' to 'carg' to fit 'clog'

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var argIndiLong = '--';
 
 $('body').terminal({
 // Main commands
-	help: function() {
+	help: function(){
 		this.echo(helpDia.innerHTML);
 	},
-	info: function() {
+	info: function(){
 		this.echo(infoDia.innerHTML);
 	},
-	social: function() {
+	social: function(){
 		this.echo('Here are all of my social media');
 		this.echo('Best way to contact me is to message me on one of these:');
 		this.echo('[[!;;;;https://www.youtube.com/channel/UCFIPFOkm5oHPcIQiVGLHCSA]YouTube]');
@@ -20,66 +20,14 @@ $('body').terminal({
 		this.echo('[[!;;;;https://open.spotify.com/user/saty1vy5b13qa7dmm3ow1fmk3?si=475487d2be904b93]Spotify]');
 		this.echo('[[!;;;;https://github.com/Pooky12332]Github]\n');
 	},
-	credits: function() {
+	credits: function(){
 		this.echo('Primary library I used to make this site:');
 		this.echo('[[!;;;;https://github.com/jcubic/jquery.terminal]https://github.com/jcubic/jquery.terminal]');
 		this.echo('The site I used for the cat picture:')
 		this.echo('[[!;;;;https://placekitten.com/]https://placekitten.com/]\n');
 	},
-	argc: function(){
-		if (argIndi == '-') {
-			argIndi = '+';
-			argIndiLong = '++';
-			this.echo('Argument Indicator is now set to \'+\'\n');
-		} else {
-			argIndi = '-';
-			argIndiLong = '--';
-			this.echo('Argument Indicator is now set to \'-\'\n');
-		}
-	},
-	fun: function(arg) {
-		if (arg != undefined) {
-  		switch (arg) {
-  			case argIndi + 'fb':
-  				for (let i = 1; i < 101; i++) {
-  					if (i % 15 == 0) {
-  						this.echo('[[i;#ebc000;]FizzBuzz]');
-  					} else if (i % 3 == 0) {
-  						this.echo('[[i;#ebc000;]Fizz]');
-  					} else if (i % 5 == 0) {
-  						this.echo('[[i;#ebc000;]Buzz]');
-  					} else {
-  						this.echo(i)
-  					}
-  				}
-  				break;
-  			case argIndiLong + 'cat':
-  				const img = $('<img src="https://placekitten.com/200/300">');
-  				this.echo(img);
-  				break;
-  			case argIndiLong + 'cat2':
-  				const img2 = $('<img src="https://placekitten.com/g/200/300">');
-  				this.echo(img2);
-  				break;
-  			case argIndiLong + 'colour':
-  				this.echo('[[;red;]I am red!]')
-  				this.echo('[[;green;]I am green!]')
-  				this.echo('[[;blue;]I am blue!]\n')
-  				break;
-  			case argIndiLong + 'embedvid':
-  				const box = $('<iframe width="560" height="315" src="https://www.youtube.com/embed/O58N8Mmv0QU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-  				this.echo(box);
-  				break;
-  			case argIndiLong + 'animtest':
-					this.echo('This is the typing animation test!\nThis is here so I can test if this animation works on other platforms\nIf you encounter a glitch with this animation\nI am probably aware of it and have it on my bug list\nSee ya!\n', { typing: true, delay: 100 });
-  				break;
-  			default:
-  				this.error('Invalid arg used!');
-  				break;
-  		}
-  	} else {
-  		this.echo(funDia.innerHTML)
-  	}
+	clog: function(){
+		this.echo(clogDia.innerHTML)
 	},
 
 // Project commands
@@ -115,18 +63,24 @@ $('body').terminal({
 	exit: function(){
 		window.close();
 	},
+	carg: function(){
+		if (argIndi == '-') {
+			argIndi = '+';
+			argIndiLong = '++';
+			document.cookie = 'ind=+'
+			this.echo('Argument Indicator is now set to \'+\'\n');
+		} else {
+			argIndi = '-';
+			argIndiLong = '--';
+			this.echo('Argument Indicator is now set to \'-\'\n');
+		}
+	},
 	date: function(){
 		const dt = new Date();
 		this.echo('The current date and time is: ' + dt + '\n');
 	},
 	echo: function(arg){
 		this.echo(arg);
-	},
-
-// Other commands
-	python: function(){
-		const box = $('<iframe src="https://trinket.io/embed/python3/7c6e42960a" width="1000" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>');
-		this.echo(box);
 	},
 	theme: function(arg){
 		var r = document.querySelector('.terminal');
@@ -155,46 +109,101 @@ $('body').terminal({
 			this.echo('-l - Change to the light theme\n-d - Change to the dark theme (defualt)\n');
 		}
 	},
-	tcol: function(arg1, arg2){
-		var r = document.querySelector('.terminal');
 
-		if (arg1 != undefined) {
-			switch (arg1) {
-				case argIndi + 'g':
-					var rs = getComputedStyle(r);
-					this.echo('Currently the text is ' + rs.getPropertyValue('--color') + '\n');
-					break;
-				case argIndi + 's':
-					r.style.setProperty('--color', arg2);
-					break;
-				default:
-					this.error('Invalid arg used!');
-					break;
-			}
-		} else {
-			this.echo('-g - Gets the current colour of text\n-s - Sets a new colour of text\n')
-		}
+// App commands
+	python: function(){
+		const box = $('<iframe src="https://trinket.io/embed/python3/7c6e42960a" width="1000" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>');
+		this.echo(box);
 	},
-	bcol: function(arg1, arg2){
-		var r = document.querySelector('.terminal');
-
+	fun: function(arg1, arg2, arg3){
 		if (arg1 != undefined) {
-			switch (arg1) {
-				case argIndi + 'g':
-					var rs = getComputedStyle(r);
+  		switch (arg1) {
+  			case argIndi + 'fb':
+  				for (let i = 1; i < 101; i++) {
+  					if (i % 15 == 0) {
+  						this.echo('[[i;#ebc000;]FizzBuzz]');
+  					} else if (i % 3 == 0) {
+  						this.echo('[[i;#ebc000;]Fizz]');
+  					} else if (i % 5 == 0) {
+  						this.echo('[[i;#ebc000;]Buzz]');
+  					} else {
+  						this.echo(i)
+  					}
+  				}
+  				break;
+  			case argIndiLong + 'cat':
+  				const img = $('<img src="https://placekitten.com/200/300">');
+  				this.echo(img);
+  				break;
+  			case argIndiLong + 'cat2':
+  				const img2 = $('<img src="https://placekitten.com/g/200/300">');
+  				this.echo(img2);
+  				break;
+  			case argIndiLong + 'colour':
+  				this.echo('[[;red;]I am red!]')
+  				this.echo('[[;green;]I am green!]')
+  				this.echo('[[;blue;]I am blue!]\n')
+  				break;
+  			case argIndiLong + 'embedvid':
+  				const box = $('<iframe width="560" height="315" src="https://www.youtube.com/embed/O58N8Mmv0QU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+  				this.echo(box);
+  				break;
+  			case argIndiLong + 'tcol':
+  				var r = document.querySelector('.terminal');
 
-					this.echo('Currently the text is ' + rs.getPropertyValue('--background') + '\n');
+					if (arg2 != undefined) {
+						switch (arg2) {
+							case argIndi + 'g':
+								var rs = getComputedStyle(r);
+								this.echo('Currently the text is ' + rs.getPropertyValue('--color') + '\n');
+								break;
+							case argIndi + 's':
+								if (arg3 != undefined) {
+									r.style.setProperty('--color', arg3);
+								} else {
+									this.error('No colour given!');
+								}
+								break;
+							default:
+								this.error('Invalid arg used!');
+								break;
+						}
+					} else {
+						this.echo('-g - Gets the current colour of text\n-s - Sets a new colour of text\n')
+					}
 					break;
-				case argIndi + 's':
-					r.style.setProperty('--background', arg2);
+				case argIndiLong + 'bcol':
+					var r = document.querySelector('.terminal');
+
+					if (arg2 != undefined) {
+						switch (arg2) {
+							case argIndi + 'g':
+								var rs = getComputedStyle(r);
+
+								this.echo('Currently the text is ' + rs.getPropertyValue('--background') + '\n');
+								break;
+							case argIndi + 's':
+								if (arg3 != undefined) {
+									r.style.setProperty('--background', arg3);
+								} else {
+									this.error('No colour given!');
+								}
+								break;
+							default:
+								this.error('Invalid arg used!');
+								break;
+						}
+					} else {
+						this.echo('-g - Gets the current colour of the background\n-s - Sets a new colour of the background\n')
+					}
 					break;
-				default:
-					this.error('Invalid arg used!');
-					break;
-			}
-		} else {
-			this.echo('-g - Gets the current colour of the background\n-s - Sets a new colour of the background\n')
-		}
+  			default:
+  				this.error('Invalid arg used!');
+  				break;
+  		}
+  	} else {
+  		this.echo(funDia.innerHTML)
+  	}
 	},
 }, {
 
@@ -225,7 +234,11 @@ $('body').terminal({
 			r.style.setProperty('--color', 'black');
 			r.style.setProperty('--background', 'white');
 			r.style.setProperty('--link-color', 'black');
-		}
+		};
+		if (document.cookie == 'ind=+') {
+			argIndi = '+';
+			argIndiLong = '++';
+		};
 	},
 
 	checkArity: false,
